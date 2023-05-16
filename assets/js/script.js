f7120a99e59dd0845f66e97a15c27469f4feea12
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-15 23:43:51 -0500

    added for loop to iterate through local storage and grab each value with a key of hour-i and set the text of the text area to that value.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,50 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   var hour = dayjs().hour();
-  console.log(hour);
   $(".time-block").each(function () {
-    console.log($(this).attr("id"));
     var id = $(this).attr("id").split("-")[1];
     var rowHour = parseInt(id);
-
-    if (hour > rowHour) {
-      $(this).addClass("past");
-    } else if (hour < rowHour) {
-      $(this).addClass("future");
-    } else {
-      $(this).addClass("present");
-    }
+    hour > rowHour
+      ? $(this).addClass("past")
+      : hour < rowHour
+      ? $(this).addClass("future")
+      : $(this).addClass("present");
   });
 
   $(".saveBtn").click(function () {
     var btnParent = $(this).parent().children(".description").val();
-    console.log(btnParent);
-    // var calendarEvent = $(".description").val();
-    // console.log("button clicked");
-    // console.log(this.parent);
-    localStorage.setItem("calendarEvent", btnParent);
-    // var calendarText = localStorage.getItem("calendarEvent", calendarEvent);
+    var btnParentAttr = $(this).parent().attr("id");
+    localStorage.setItem(btnParentAttr, btnParent);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  for (let i = 0; i < 18; i++) {
+    var calendarText = localStorage.getItem("hour-" + [i]);
+    $("#hour-" + [i])
+      .children(".description")
+      .text(calendarText);
+  }
 });
+
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
 
 // anthony said use military time to make it simple for am and pm
 //
