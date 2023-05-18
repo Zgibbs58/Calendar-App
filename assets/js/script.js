8d89e9c16ae4c64d758ec33765bd1c550668136d
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-17 22:30:21 -0500

    got rid of the calendarText variable since it was only used once and put it's value in description text. Also added notes to explain code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
 $(function () {
   // pulling the current day from dayjs and using it to populate header p text
   $("#currentDay").text(dayjs().format("MMM D, YYYY"));
-  var hour = dayjs().hour() -3;
+  var hour = dayjs().hour();
   $(".time-block").each(function () {
+    // id using this to grab the (.time-block) id attribute and split it into an array of two items. ["hour", "9"] then using index of 1 to grab second string 9.
     var id = $(this).attr("id").split("-")[1];
     var rowHour = parseInt(id);
+    // ternary operator used to set the class for text block color based on current hour
     hour > rowHour
       ? $(this).addClass("past")
       : hour < rowHour
       ? $(this).addClass("future")
       : $(this).addClass("present");
-      var calendarText = localStorage.getItem(id);
-      $(this).children(".description").text(calendarText);
+      // sets the text of the child of timeblock with a class of description to the local storage variable. 
+      $(this).children(".description").text(localStorage.getItem(id));
       
     //   var calendarText = localStorage.getItem(id);
     // $(id).children(".description").text(calendarText);
@@ -22,14 +24,6 @@ $(function () {
     var hourTxt = $(this).parent().children(".description").val();
     localStorage.setItem(hourAttr, hourTxt);
   });
-  // for loop to iterate through local storage change text of the correlated value of the hour
-  // for (let i = 0; i < 18; i++) {
-  //   var calendarText = localStorage.getItem("hour-" + [i]);
-  //   $("#hour-" + [i])
-  //     .children(".description")
-  //     .text(calendarText);
-  //     console.log(calendarText)
-  // }
 });
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
